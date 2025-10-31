dd7c6f016def835fdb5a7c5c92e9ece0c031a1df
Kalkulai <kalkuali.tech@gmail.com>
2025-10-31 13:04:24 +1100

    testing hf and cloudflare

--- backend/main.py
+++ backend/main.py
@@ -864,7 +864,3 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", "7860"))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
-
-
-
-#this is for testing deployment to hf
